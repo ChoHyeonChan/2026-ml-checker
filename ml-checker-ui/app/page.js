4a8f80dab0023c5f1b1cd481496c0228fc384745
Cho Hyeon Chan <203729688+ChoHyeonChan@users.noreply.github.com>
2026-09-13 20:11:12 +0900

    feat: 코드 영역 JSX

--- ml-checker-ui/app/page.js
+++ ml-checker-ui/app/page.js
@@ -1,1 +1,7 @@
-
+          <textarea
+            className={styles.codeArea}
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            placeholder={`pandas, sklearn 등을 쓰는 전처리 코드를 붙여넣으세요.`}
+          />
+        </div>
